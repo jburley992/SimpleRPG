10d8f7060381ca6d4a9248eeb491584e6c954a21
Jacob <jburley992@gmail.com>
2018-06-26 21:17:49 -0400

    Refactor.2

--- NPC.py
+++ NPC.py
@@ -1,5 +1,5 @@
 # Design enemy so that it Can be part of a sprite Group
-import pygame
+import pygame,random
 
 class NPC(pygame.sprite.Sprite):
     def __init__(self,image):
@@ -11,5 +11,34 @@ class NPC(pygame.sprite.Sprite):
 
 class Enemy(NPC):
 
-    def __init__(self,image):
+    def __init__(self,image,damage,health,*abilities):
         NPC.__init__(image)
+        self.base_damage = damage
+        self.health = health
+        self.abilities = abilities
+    #Buff allies, harm foes
+    def battle(self,allies=None,foes=None):
+        '''Fighting Mechanics will go here
+           Devise Algorithm to make attacks seem pseudorandom
+           Additionally, attacks with more damage should be used less ofter'''
+        tmp = 0
+        for atk in self.abilities:
+            tmp += atk.damage
+        counter = 0
+        for atk in self.abilities:
+            atk.likelihood = atk.damage/tmp
+            if atk.inflictStatus == True:
+                counter += 0.1*atk.likelihood
+                atk.likelihood -= 0.1*atk.likelihood
+            #Basic Attack gets an increase in likelihood
+            #Over Strong and Special attacks
+            self.abilities[0] += counter
+            id = float(random.randint(0,1000))/1000.0
+        for index,atk in enumerate(self.abilities):
+            if atk.likelihood < id and not (self.abilities[index + 1]  <id):
+                return atk
+
+
+
+
+        pass
